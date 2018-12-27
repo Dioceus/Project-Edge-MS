daad59522b5a4ac1e1561152bb466e2a8803f6c3
David Chen <chuwei.chen2001@gmail.com>
2018-12-27 00:05:24 -0500

    Segmentation algorithm done

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 import json
 from shutil import rmtree
 from analyze import get_average_lightness
+from segment import segment_division 
+from segment import Segment 
 from PIL import Image 
-
 
 """
 1. Take image
@@ -19,6 +20,7 @@ Problem:
 2. Possibly the fact that the spots are not the only white things, so the image needs to check with surroundings
 
 """
+
 def main(path, filename):
      
     input_image = Image.open(path) 
@@ -28,6 +30,16 @@ def main(path, filename):
 
     # Init variables, determine average lightness of the image 
     avg_lightness = get_average_lightness(input_image, 0, 0, width, height)
+
+    num_across = width / segment_division(input_image)[0]
+    num_down = height / segment_division(input_image)[1]
+    single_segments = []
+    segment_width = segment_division(input_image)[0] 
+    segment_height = segment_division(input_image)[1] 
+    for i in range(num_across):
+         for j in range(num_down):
+              single_segments.append(Segment(input_image, i * segment_width, j * segment_height, segment_width, segment_height)) 
+    
     
 
 
--- segment.py
+++ segment.py
@@ -1,23 +1,36 @@
 import pygame
-from PIL import Image
+from PIL import Image 
 
-def _init_(self, input_image):
-    self.image = pygame.image.load(input_image).convert()
-
-#def get_segment(self):
-    
-
-def segment_division(input_image.size):
-    if (input_image.size<3):
-        return [input_image.size, 1]
+def segment_division(input_image):
+    size = input_image.size
+    """
+    if size < 3:
+        return [size, 1]
+    """
     result = list()
-    for i in range(1, int(input_image.size ** 0.5) + 1):
-       div, mod = divmod(input_image.size, i)
+    for i in range(1, int(size ** 0.5) + 1):
+       div, mod = divmod(size, i)
        #ignore 1 and n itself as factors
-       if mod == 0 and i != 1 and div != input_image.size:
+       if mod == 0 and i != 1 and div != size:
            result.append(div)
            result.append(i)
     if len(result)==0: # if no factors then add 1
-        return divide_equally(input_image.size+1)
+        return segment_division(size+1)
     return result[len(result)-2:]
+    #segment_division[0] is width and segment_division[1] is height 
+
+class Segment:
+    #image = input_image
+    #width = 0
+    #height = 0
+    def _init_(self, input_image, x, y, width, height):
+        self.image = pygame.image.load(input_image).convert()
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height 
+        
+
+
+
 
